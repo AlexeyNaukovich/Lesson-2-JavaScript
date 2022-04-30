c5388dd51059384c2d07331348550fe53a487b74
AlexeyNaukovich///QA-Courses-of-Vadim-Ksendzov <94318312+AlexeyNaukovich@users.noreply.github.com>
2022-04-30 16:50:40 +0300

    Lesson1 Java Script

---
+++ Lesson1.js
@@ -0,0 +1,65 @@
+let yolochka4 = 4;
+let yolochka6 = 6;
+
+let result = yolochka4 + yolochka6;
+
+console.log(result);
+console.log("HELLO");
+console.log("HELLO");
+console.log("HELLO");
+console.log("HELLO");
+let yolochka_name = "zelyonaya"
+
+let resalt = yolochka4 = yolochka6;
+let result_2 = yolochka6 + yolochka_name;
+
+
+
+
+console.log(yolochka4 + yolochka6);
+
+console.log(result);
+console.log(result_2);
+
+
+
+let t = true;
+let f = false;
+
+console.log(t + f);
+
+if (f) {
+    let apples = 3+5
+    console.log("if true = " + apples)
+}
+    else if (true){
+        let milk = 2
+        console.log("else if = " + milk)
+} else {
+    let meat = 2+2
+    console.log("if not true = " + meat)
+}
+
+
+
+
+let age = 18;
+
+let tt = typeof age
+console.log(tt)
+
+
+if (typeof(age) =="number") {
+if (age >= 18 && age <60) {
+
+console.log("full")
+}
+else if (age < 18 ){
+
+    console.log("mult")
+}
+else if (age >= 60) {
+    console.log("kult")
+}
+} else {
+    console.log("good")
