c3b4025d46d9290f76f39e2a1e0d4593b5f42dce
AlexeyNaukovich <ww1794@yandex.ru>
2022-04-30 16:24:30 +0300

    HomeWorkJS1

---
+++ HomeWorkJS1.js
@@ -0,0 +1,99 @@
+let item_1 = 5                                          // 1-2 Пункты
+console.log(item_1)                                     // 3 Пункт
+
+let item_2 = 3                                          // 4-5 Пункты
+console.log(item_2)                                     // 6 Пункт
+
+let item_3 = item_1 + item_2                            // 7-8 Пункты
+console.log(item_3)                                     // 9 Пункт
+
+let item_4 = "yolochka"                                 // 10-11 Пункты
+console.log(item_4)                                     // 12 Пункт
+
+console.log(item_3 + item_4)                            // 13 Пункт
+console.log(item_3*item_4)                              // 14 Пункт
+
+let item_5 = item_3                                     // 15-16 Пункты
+let item_6 = 15                                         // 17-18-19-20 Пункты
+let item_6_type = Number(item_6)                        // 17-18-19-20 Пункты
+console.log(typeof item_6_type)                         // 17-18-19-20 Пункты
+
+console.log("item_6 ==", item_6,"item_6_type == ",typeof item_6 )                   // 21 Пункт
+
+let item_7 = String(item_6)                             // 22 Пункт
+console.log(typeof item_7)                              // 22 Пункт
+
+
+let item_7_type = typeof item_7                         // 23-24 Пункты
+console.log(item_7_type)                                // 23-24 Пункты
+
+console.log("item_7 ==", item_7, "item_7_type == ", typeof item_7 )                 // 25 Пункт
+
+let age_1 = 10                                          // 26 Пункт
+let age_2 = 18                                          // 27 Пункт
+let age_3 = 60                                          // 28 Пункт
+
+if (age_1 < age_2)                                      // 29-30 Пункты
+{
+    console.log("You dont have access cause your age is "+ age_1 + " Its less then")    // 29-30 Пункты
+}
+
+if (age_1>=age_2 + age_1<age_3)                         // 31 Пункт
+{
+    console.log("Welcome!!!!!")                         // 31 Пункт
+}
+
+if ( age_1>age_3)                                       // 32 Пункт
+{                                      
+    console.log("Keep calm and look Culture channel")   // 32 Пункт
+}
+else if(age_1<age_3)                                    // 33 Пункт
+{                                  
+    console.log("Technical work")                       // 33 Пункт
+}
+
+const checkAge = funktion(Age)
+if (age_1 < age_2)                                      // 29-30 Пункты
+{
+    console.log("You dont have access cause your age is "+ age_1 + " Its less then")    // 29-30 Пункты
+}
+
+if (age_1>=age_2 + age_1<age_3)                         // 31 Пункт
+{
+    console.log("Welcome!!!!!")                         // 31 Пункт
+}
+
+if ( age_1>age_3)                                       // 32 Пункт
+{                                      
+    console.log("Keep calm and look Culture channel")   // 32 Пункт
+}
+else if(age_1<age_3)                                    // 33 Пункт
+{
+    console.log("Technical work")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
